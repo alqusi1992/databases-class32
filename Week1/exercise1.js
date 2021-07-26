0855fd11ece140ec56308ac952914d9588575788
malqusi <malqusi@yahoo.com>
2021-07-26 20:29:18 +0200

    Database homework week1

---
+++ Week1/exercise1.js
@@ -0,0 +1,82 @@
+var mysql = require("mysql");
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "hyfuser",
+  password: "hyfpassword",
+  database: "company",
+});
+
+// Connection with database
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+
+  con.query("DROP DATABASE meetup");
+
+  /* Create a database named "meetup": */
+  con.query("CREATE DATABASE meetup", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
+});
+
+// Create Invitee table
+
+con.query("DROP TABLE invitee");
+
+const invitee_sql =
+  "CREATE TABLE invitee (invitee_no INT, invitee_name  VARCHAR(50), invited_by  VARCHAR(50))";
+con.query(invitee_sql, function (err, result) {
+  if (err) throw err;
+  console.log("Invitee Table created");
+});
+
+// Create Room table
+con.query("DROP TABLE room");
+
+const room_sql =
+  "CREATE TABLE room (room_no INT,  room_name  VARCHAR(50), floor_number INT)";
+con.query(room_sql, function (err, result) {
+  if (err) throw err;
+  console.log("room Table created");
+});
+
+// Create meeting table
+con.query("DROP TABLE meeting");
+
+const meeting_sql =
+  "CREATE TABLE meeting (meeting_no INT, meeting_title TEXT, starting_time TIMESTAMP, ending_time TIMESTAMP, room_no INT)";
+con.query(meeting_sql, function (err, result) {
+  if (err) throw err;
+  console.log("meeting Table created");
+});
+
+// Data
+
+// Insert Invitee Data
+
+const invitee_data =
+  "INSERT INTO invitee VALUES (1, 'Sam', 'Hans'),(2, 'Yara', 'Mo'),(3, 'Rafe', 'Majdolen'),(4, 'Mayez', 'Ali'),(5, 'Waseem', 'Anas')";
+con.query(invitee_data, function (err, result) {
+  if (err) throw err;
+  console.log("Invitee data has been inserted");
+});
+
+// Insert Room Data
+
+const room_data =
+  "INSERT INTO room VALUES (101, 'Study', 1),(102, 'Spare', 2),(103, 'Nursery', 3),(104, 'Utility', 4),(105, 'Loft', 5)";
+con.query(room_data, function (err, result) {
+  if (err) throw err;
+  console.log("Room data has been inserted");
+});
+
+// Insert meeting Data
+
+const meeting_data =
+  "INSERT INTO meeting VALUES (1, 'A Celebration of Success', '2021-05-25 15:20:30','2021-05-25 18:20:30', 101),(2, 'Focus on Success', '2022-01-25 18:40:00','2022-01-26 20:40:00', 102),(3, 'Social', '2022-04-13 14:00:00','2022-04-13 15:00:00', 103),(4, 'Exams', '2021-03-20 14:00:00', '2021-03-21 15:00:00',104),(5, 'Dating', '2023-02-14 18:00:00', '2023-02-14 20:00:00', 105)";
+con.query(meeting_data, function (err, result) {
+  if (err) throw err;
+  console.log("meeting data has been inserted");
+});
---
+++ Week1/world.js
@@ -0,0 +1,106 @@
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "hyfuser",
+  password: "hyfpassword",
+  database: "world",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+});
+
+connection.query("use world", (err, result) => {
+  if (err) throw err;
+  console.log(result);
+});
+// 1.What are the names of countries with population greater than 8 million?
+connection.query(
+  "SELECT name from country WHERE population > 8000000",
+  (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  }
+);
+
+// 2.What are the names of countries that have “land” in their names?
+
+connection.query(
+  "SELECT name from country WHERE name LIKE '%land'",
+  (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  }
+);
+
+// 3.What are the names of the cities with population in between 500,000 and 1 million?
+
+connection.query(
+  "SELECT name FROM city WHERE population BETWEEN 500000 AND 1000000",
+  (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  }
+);
+
+// 4.What's the name of all the countries on the continent ‘Europe’?
+
+connection.query(
+  "SELECT * FROM country WHERE continent = 'Europe'",
+  (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  }
+);
+
+// 5.List all the countries in the descending order of their surface areas.
+connection.query(
+  "SELECT * FROM country ORDER BY SurfaceArea DESC",
+  (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  }
+);
+
+// 6. What are the names of all the cities in the Netherlands?
+connection.query(
+  "SELECT Name FROM city WHERE CountryCode='NLD'",
+  (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  }
+);
+
+// 7. What is the population of Rotterdam?
+connection.query(
+  "SELECT Population from city WHERE name = 'Rotterdam'",
+  (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  }
+);
+
+// 8.What's the top 10 countries by Surface Area?
+connection.query(
+  "SELECT * FROM country ORDER BY SurfaceArea DESC LIMIT 10",
+  (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  }
+);
+
+//9. What's the top 10 most populated cities?
+connection.query(
+  "SELECT * FROM city ORDER BY Population  DESC LIMIT 10",
+  (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  }
+);
+
+// 10. What is the population number of the world?
+
+connection.query("SELECT SUM(population) FROM country", (err, result) => {
+  if (err) throw err;
+  console.log(result);
+});
